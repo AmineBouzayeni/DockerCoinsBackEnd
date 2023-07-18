3c2be6dffd5e28d03d4278832ac1d03ba1816616
Amine Bouzayeni <mohamed-amine.bouzayeni@teolia.fr>
2023-07-18 18:38:35 +0200

    Add redis pipeline and smoke tests

---
+++ redis_server/tests/smoke_test.py
@@ -0,0 +1,8 @@
+import pytest
+import requests
+from redis import Redis
+
+def test_insert_object_and_read_it():
+    redis_server = Redis(host=__URI__, port=30003)
+    redis_server.set('foo', 'bar')
+    assert redis_server.get('foo') == "b'bar'"
