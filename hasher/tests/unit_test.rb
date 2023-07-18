dca0b1d3a7375e51b73f38a99f49a04bdd5f0eb0
Amine Bouzayeni <mohamed-amine.bouzayeni@teolia.fr>
2023-07-18 17:57:51 +0200

    Update the hasher pipeline

--- hasher/tests/unit_test.rb
+++ hasher/tests/unit_test.rb
@@ -5,11 +5,11 @@ require "net/http"
 class CustomTest < Test::Unit::TestCase
 
     def test_gethostname
-      res = Net::HTTP.get_response(URI(__uri__))
+      res = Net::HTTP.get_response(URI(__URI__))
       assert_equal("200", res.code, 'Get hostname endpoint is working.')
     end
     def test_hash_with_random
-      res = Net::HTTP.post(URI(__uri__+"/random"))
+      res = Net::HTTP.post(URI(__URI__+"/random"))
       assert_equal("200", res.code, 'Post hash endpoint is working.')
     end  
 end
