102a5a5093a8df48984c285dafdbca6a729c7806
Amine Bouzayeni <mohamed-amine.bouzayeni@teolia.fr>
2023-07-19 12:00:00 +0200

    - Update GKE connection method. - Reformat and add some env variables to the workflows. - Update the targeted deployment node to the new dedicated 'test' node   pool.

--- redis_server/tests/smoke_test.py
+++ redis_server/tests/smoke_test.py
@@ -3,6 +3,6 @@ import requests
 from redis import Redis
 
 def test_insert_object_and_read_it():
-    redis_server = Redis(host=__URI__, port=30003)
+    redis_server = Redis(host=__URI__, port=__PORT__)
     redis_server.set('foo', 'bar')
     assert redis_server.get('foo') == b'bar'
